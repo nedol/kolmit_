7cd15bb482f24076c8b7405cd44ef4fee6629d30
nedol <nedooleg@gmail.com>
2023-02-07 16:15:02 +0100

    video

---
+++ src/user/Video.local.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { onMount } from 'svelte';
+    export let display = 'none';
+    export let srcObject ='';
+    let lv;
+    onMount(async () => {
+
+    });
+
+    $: if(lv && srcObject){
+        lv.srcObject = srcObject;
+    }else if(lv && lv.srcObject){
+        lv.srcObject.getVideoTracks().forEach(track => {
+            track.stop()
+            lv.srcObject.removeTrack(track);
+        });
+        lv.src='';
+    }
+
+</script>
+
+<video  bind:this = {lv} id="localVideo" autoplay playsinline muted style="
+        display: {display};
+        position: absolute;
+        bottom:0px;
+        right:0px;   
+        max-height: 100%;
+        z-index: 20;">
+</video>
